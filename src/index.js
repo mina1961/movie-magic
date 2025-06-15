257eaa449d2e192616a2679033a45b4e2490c8bd
mina bergova <bergovam@gmail.com>
2025-06-15 08:35:58 +0300

    setup express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Just works!')
+})
+
+app.listen(5000, () => console.log('Server is listening at http://localhost:5000...'));
